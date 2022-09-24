1b7fc00887820c4ca958847a3d963bb45b5fa2bd
haiduy <86014550+hai232@users.noreply.github.com>
2022-09-25 02:45:06 +0700

    Update tool_evo.js

--- tool_evo.js
+++ tool_evo.js
@@ -23,28 +23,6 @@ g.style.fontSize = "50px";
 g.style.backgroundColor = 'powderblue'
 g.style.top = '0px'
 document.body.appendChild(g);
-let x = 0;
-let y = 0;
-const ele = document.getElementById('dragMe');
-const mouseDownHandler = function (e) {
-    x = e.clientX;
-    y = e.clientY;
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-const mouseMoveHandler = function (e) {
-    const dx = e.clientX - x;
-    const dy = e.clientY - y;
-    ele.style.top = `${ele.offsetTop + dy}px`;
-    ele.style.left = `${ele.offsetLeft + dx}px`;
-    x = e.clientX;
-    y = e.clientY;
-};
-const mouseUpHandler = function () {
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-};
-ele.addEventListener('mousedown', mouseDownHandler);
     statistic = []
     score = 0;
     num = 0;
