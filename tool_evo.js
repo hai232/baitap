7f7d6181603c1bef79d3ea4fbfb84f37223c2101
haiduy <86014550+hai232@users.noreply.github.com>
2022-09-25 02:13:43 +0700

    Update tool_evo.js

--- tool_evo.js
+++ tool_evo.js
@@ -1,1 +1,55 @@
-alert(123)
+
+    g = document.createElement('div');
+    g.setAttribute("id", "dragMe");
+    g.setAttribute("class", "draggable");
+    g.style.cursor = "move";
+    g.innerText = 123123
+    g.style.position = "absolute";
+    g.style.zIndex = 1000;
+    g.style.height = "250px";
+    g.style.width = "350px";
+    g.style.opacity = 0.85;
+    g.style.fontSize = "50px";
+    g.style.backgroundColor = 'powderblue'
+    document.body.appendChild(g);
+
+    // The current position of mouse
+let x = 0;
+let y = 0;
+
+// Query the element
+const ele = document.getElementById('dragMe');
+
+// Handle the mousedown event
+// that's triggered when user drags the element
+const mouseDownHandler = function (e) {
+    // Get the current mouse position
+    x = e.clientX;
+    y = e.clientY;
+
+    // Attach the listeners to `document`
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - x;
+    const dy = e.clientY - y;
+
+    // Set the position of element
+    ele.style.top = `${ele.offsetTop + dy}px`;
+    ele.style.left = `${ele.offsetLeft + dx}px`;
+
+    // Reassign the position of mouse
+    x = e.clientX;
+    y = e.clientY;
+};
+
+const mouseUpHandler = function () {
+    // Remove the handlers of `mousemove` and `mouseup`
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+};
+
+ele.addEventListener('mousedown', mouseDownHandler);
